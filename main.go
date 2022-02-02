974fec0d3568f1b0f42faae788f3a36f8cb1a1ba
sujan <sujanb.poojary@gmail.com>
2022-02-02 21:39:04 +0530

    Hello world script added

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World.!")
+}
+
+
+/* 
+ 
+	main  - executable file
+	more specific package name - reusable package / dependency type package
+
+*/
