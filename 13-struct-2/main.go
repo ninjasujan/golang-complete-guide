36bd2b5c3309f9d628d2dbfe2d15a0f33cb38fe3
sujan <sujanb.poojary@gmail.com>
2025-06-09 18:31:07 +0530

    Add main.go and go.mod for struct demonstration; implement createPerson function and showcase struct usage with address and social media details

---
+++ 13-struct-2/main.go
@@ -0,0 +1,61 @@
+package main
+
+import "fmt"
+
+func main() {
+	person := createPerson("Sujan", "sujan@gmail.com", struct {
+		City    string
+		PinCode uint16
+		Country string
+	}{
+		City:    "Kathmandu",
+		PinCode: 44600,
+		Country: "Nepal",
+	}, struct {
+		Facebook  string
+		Instagram string
+		Twitter   string
+	}{
+		Facebook:  "facebook",
+		Instagram: "instagram",
+		Twitter:   "twitter",
+	})
+
+	// Now you can access the fields
+	fmt.Printf("Person: %+v\n", person)
+	fmt.Printf("Name: %s\n", person.Name)
+	fmt.Printf("City: %s\n", person.Address.City)
+	fmt.Printf("Instagram: %s\n", person.SocialMedia.Instagram)
+}
+
+func createPerson(name string, email string, address struct {
+	City    string
+	PinCode uint16
+	Country string
+}, socialMedia struct {
+	Facebook  string
+	Instagram string
+	Twitter   string
+}) *Person {
+	return &Person{
+		Name:        name,
+		Email:       email,
+		Address:     address,
+		SocialMedia: socialMedia,
+	}
+}
+
+type Person struct {
+	Name    string
+	Email   string
+	Address struct {
+		City    string
+		PinCode uint16
+		Country string
+	}
+	SocialMedia struct {
+		Facebook  string
+		Instagram string
+		Twitter   string
+	}
+}
